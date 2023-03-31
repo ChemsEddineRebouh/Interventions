f4d9087277f4a19250c14d60f011d646b09ee02f
Chems-Eddibe Rebouh <drebouh123@gmail.com>
2023-03-31 15:42:51 -0400

    Tp6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProblemeComponent } from './probleme/probleme.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'acceuil',component:AccueilComponent},
+  {path:'probleme',component:ProblemeComponent},
+  {path:'',redirectTo:'acceuil',pathMatch:'full'},
+  {path:'**',redirectTo:'acceuil',pathMatch:'full'}, // si la route est inexistante rediriger l'user
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
